97dd103e5e929f27a49b256f001e513ba49d2dd9
achchuthan <achch1990@gmail.com>
2021-01-23 19:27:41 +0530

    Registration error modified and vendor script move to auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApplicationRegistration;
+use App\Models\Role;
+use App\Models\Student;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -70,5 +77,60 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $student = Student::where([['nic',strtoupper(trim($request['nic']))],['al_index_number',$request['al']]])->first();
+        if(!$student)
+            return back()->withInput()->with(['warning'=>'The Index Number is incorrect or you are not selected for university admission']);
+        $enroll = $student->enrolls()->latest()->first();
+
+        $application = ApplicationRegistration::where([['academic_year_id',$enroll->academic_year_id],['programme_id',$enroll->programme_id]])->first();
+
+        if(!$application){
+            return back()->with(['error'=>'Database Error: Requested application for the course is not in the database'])->withInput();
+        }
+        if(!$enroll){
+            return back()->with(['error'=>'Database Error: The Student did not enroll to any course.'])->withInput();
+        }
+        $open_date = Carbon::parse($application->open_date);
+        $close_date = Carbon::parse($application->close_date);
+        $now = Carbon::now();
+        $now->hour = 0;
+        $now->minute = 0;
+        $now->second = 0;
+
+        if(!(($now->greaterThanOrEqualTo($open_date) && $now->lessThanOrEqualTo($close_date)) || $application->status=='Draft') ){
+//            return response()->json([$now,$now->greaterThanOrEqualTo($open_date),$now->lessThanOrEqualTo($close_date)]);
+            return back()->with(['info'=>'Application will be open soon/Please Contact Officials'])->withInput();
+        }
+        if($student->users()->get()->count()>0){
+            return back()->with(['warning'=>'User already Exist with your data. If you are not created already please contact Officalis'])->withInput();
+        }
+        $user = $this->create($request->all());
+        $role_student = Role::where('name', 'Student')->first();
+        $user->roles()->attach($role_student);
+        $user->students()->attach($student);
+        $student->email = $user->email;
+        $student->update();
+        event(new Registered($user));
+        $this->guard()->login($user);
+
+        if ($response = $this->registered($request, $user)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 201)
+            : redirect()->route('student.registration.index');
     }
 }
