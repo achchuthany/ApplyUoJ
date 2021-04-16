b4dc5c36f2eed76ac732449b642cfaebbfeb242a
achchuthan <achch1990@gmail.com>
2021-04-16 21:12:56 +0530

    Letter "A" removed

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -16,22 +16,22 @@ class RolesTableSeeder extends Seeder
     {
         $role_user = new Role();
         $role_user->name = 'Student';
-        $role_user->description = 'A normal Student';
+        $role_user->description = 'Normal Student';
         $role_user->save();
 
         $role_lecturer = new Role();
         $role_lecturer->name = 'Welfare';
-        $role_lecturer->description = 'A welfare Office';
+        $role_lecturer->description = 'Welfare Office';
         $role_lecturer->save();
 
         $role_hod = new Role();
         $role_hod->name = 'Dean';
-        $role_hod->description = 'A Dean of Faculty';
+        $role_hod->description = 'Dean of Faculty';
         $role_hod->save();
 
         $role_admin = new Role();
         $role_admin->name = 'Admin';
-        $role_admin->description = 'A Admin';
+        $role_admin->description = 'Administrator';
         $role_admin->save();
     }
 }
